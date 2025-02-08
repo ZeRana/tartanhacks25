4229d4749ef8ab1e22901b2f552a411b095ed8a3
garrison2 <garrison.chan224@gmail.com>
2025-02-07 22:44:24 -0500

    completed GameObject

--- GameObject.py
+++ GameObject.py
@@ -5,21 +5,26 @@ from pygame import Surface
 rootPath = os.getenv('GAMEPATH')
 
 class GameObject(Sprite):
-    def __init__(self, image):
+    def __init__(self, surface):
         super().__init__();
 
-        self.image = image
+        self.surface = surface
         self.x = 0
         self.y = 0
 
     def isClicked(self, x, y):
-        pass
-
+        if self.surface.isRect: 
+            return (self.x <= x <= self.x + self.surface.width and
+                    self.y <= y <= self.y + self.surface.height)
+        else:
+           obj_mask = pygame.mask.from_surface(self.surface)
+           return obj_mask.get_at((x - self.x, y - self.y)) == 1
+               
     def interact(self, **kwargs):
         pass
 
     def draw(self, screen):
-        screen.blitz(self.image, (self.x, self.y))
+        screen.blitz(self.surface, (self.x, self.y))
 
     def setPosition(self, x, y):
         self.x = x
@@ -27,15 +32,15 @@ class GameObject(Sprite):
         
 class SpriteSurface(Surface):
     def __init__(self, spriteVal,
-                 length = 30, width = 30, color = (255, 255, 255),
+                 width = 30, height = 30, color = (255, 255, 255),
                  imgDir = None, **kwargs):
-        self.length = length
         self.width = width
+        self.height = height
         self.isRect = True
 
         if isinstance(spriteVal, str):
             if spriteVal == 'default':
-                super().__init__((self.length, self.width))
+                super().__init__((self.width, self.height))
                 self.fill(color)
                 return
 
@@ -54,7 +59,7 @@ class SpriteSurface(Surface):
         elif not isinstance(spriteVal, Surface):
             raise Exception(f'invalid surface {spriteVal}')
 
-        self.length, self.width = spriteVal.get_size()
-        super().__init__((self.length, self.width))
+        self.width, self.height = spriteVal.get_size()
+        super().__init__((self.width, self.height))
         self.blit(spriteVal, (0, 0))
         self.isRect = False
